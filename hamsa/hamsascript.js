4c86ee6008fd58ce151bcd44ae1f493af2c4363f
kailafitz <mikhailafitzpatrick@yahoo.com>
2020-08-19 21:02:31 +0100

    Updated Bootstrap website with Hamsa and SCSS files

--- hamsa/hamsascript.js
+++ hamsa/hamsascript.js
@@ -1,28 +1,42 @@
+    // Get modal element
+    var modal = document.getElementById('in-modal');
+
+    // Get open modal button
+    var modalBtn = document.getElementById('login-btn');
+
+    // Get close button 
+    var closeBtn = document.getElementsByClassName('clsbtn')[0];
+
+    // Listen for open click
+    modalBtn.addEventListener('click', openModal);
+
+    // Listen for close click
+    closeBtn.addEventListener('click', closeModal);
+
+    // Function to open modal
+    function openModal() {
+        // console.log(123);
+        modal.style.display='block';
+    }
+
+    // Function to open modal
+    function closeModal() {
+        // console.log(123);
+        modal.style.display='none';
+    }
 
 
-// Get modal element
-var modal = document.getElementById('in-modal');
+$(document).ready(function() {
+    var bars = $("#menu-bars i");
 
-// Get open modal button
-var modalBtn = document.getElementById('mdl-btn');
+    $(bars).click(function() {
 
-// Get close button 
-var closeBtn = document.getElementsByClassName('clsbtn')[0];
+        // $("#mobile-nav").css("display", "block");
+        $("#mobile-nav").slideToggle();
+    });
 
-// Listen for open click
-modalBtn.addEventListener('click', openModal);
-
-// Listen for close click
-closeBtn.addEventListener('click', closeModal);
-
-// Function to open modal
-function openModal() {
-    // console.log(123);
-    modal.style.display='block';
-}
-
-// Function to open modal
-function closeModal() {
-    // console.log(123);
-    modal.style.display='none';
-}
+    var close = $("#mobile-clsbtn");
+    $(close).click(function() {
+        $("#mobile-nav").slideToggle();
+    });
+});
